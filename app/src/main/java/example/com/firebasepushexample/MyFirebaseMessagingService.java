b831a18fd553393fcefff4f5d43f89db7a720d6f
Dhanaprabhu <dhanaprabhu.inxsasia.com>
2018-12-28 14:53:14 +0530

    Initial commit

---
+++ app/src/main/java/example/com/firebasepushexample/MyFirebaseMessagingService.java
@@ -0,0 +1,48 @@
+package example.com.firebasepushexample;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        /*//if the message contains data payload
+        //It is a map of custom keyvalues
+        //we can read it easily
+        if(remoteMessage.getData().size() > 0){
+            //handle the data message here
+        }
+
+        //getting the title and the body
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        //then here we can use the title and body to build a notification*/
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        //String channelId = "Default";
+        NotificationCompat.Builder builder = new  NotificationCompat.Builder(this, Constants.CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("test")
+                .setContentText(remoteMessage.getNotification().getBody()).setAutoCancel(false).setContentIntent(pendingIntent);;
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(Constants.CHANNEL_ID, Constants.CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+        manager.notify(0, builder.build());
+    }
+}
